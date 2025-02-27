a5a93d541d1b63a0a2966fa20676e8e30f5c7593
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-27 11:40:31 -0500

    Finishing demo app.

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,5 +1,19 @@
 // dominio.com/news
 
+import Link from "next/link";
+
 export default function NewsPage() {
-    return <h1>The News Page</h1>
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 }
