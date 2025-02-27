8d28eb076031b34a0469860fd1f0b46da6d697f4
diegomayorga <diegomayorga312000@gmail.com>
2025-02-27 15:06:32 -0500

    First commit.

---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+// // dominio.com/
+
+export default function HomePage() {
+    return <h1>The Home Page</h1>
+}
---
+++ pages/news.jsx
@@ -0,0 +1,5 @@
+// dominio.com/news
+
+export default function NewsPage() {
+    return <h1>The News Page</h1>
+}
